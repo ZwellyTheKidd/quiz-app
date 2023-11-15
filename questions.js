7a52bd380756815d1de737f1670599f3f5ae8520
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-11-15 16:35:43 +0200

    quiz  app

---
+++ questions.js
@@ -0,0 +1,29 @@
+questions = [
+    {
+        question: "Who is the first president of SA?",
+        options: [
+            { id: 'o1', option: 'Jacob Zuma', isCorrect: false },
+            { id: 'o2', option: 'Julius Malema', isCorrect: false },
+            { id: 'o3', option: 'Thabo Mbeki', isCorrect: true },
+            { id: 'o4', option: 'Helen Zihle', isCorrect: false },
+        ]
+    },
+    {
+        question: "2 + 2",
+        options: [
+            { id: 'o1', option: '3', isCorrect: false },
+            { id: 'o2', option: '4', isCorrect: true },
+            { id: 'o3', option: '5', isCorrect: false },
+            { id: 'o4', option: '44', isCorrect: false },
+        ]
+    },
+    {
+        question: "What's the best soccer team in south africa",
+        options: [
+            { id: 'o1', option: 'Chief', isCorrect: false },
+            { id: 'o2', option: 'Sundowns', isCorrect: false },
+            { id: 'o3', option: 'Pirate', isCorrect: true },
+            { id: 'o4', option: 'Amazulu', isCorrect: false },
+        ]
+    },
+]
