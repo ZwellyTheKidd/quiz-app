ac10ff0e8570ed99050882eec92d381c46b077cd
ZwellyTheKidd <zwelly.thekidd@outlook.com>
2023-11-16 16:02:44 +0200

    quiz  app

--- questions.js
+++ questions.js
@@ -9,39 +9,65 @@ questions = [
         ]
     },
     {
-        question: "2 + 2",
+        question: "What does “www” stand for in a website browser?",
         options: [
             { id: 'o1', option: '3', isCorrect: false },
-            { id: 'o2', option: '4', isCorrect: true },
+            { id: 'o2', option: 'World Wide Web', isCorrect: true },
             { id: 'o3', option: '5', isCorrect: false },
             { id: 'o4', option: '44', isCorrect: false },
         ]
     },
     {
-        question: "What's the best soccer team in south africa",
+        question: "What's the best football team in south africa",
         options: [
-            { id: 'o1', option: 'Chief', isCorrect: false },
+            { id: 'o1', option: 'Chiefs', isCorrect: false },
             { id: 'o2', option: 'Sundowns', isCorrect: false },
             { id: 'o3', option: 'Pirate', isCorrect: true },
             { id: 'o4', option: 'Amazulu', isCorrect: false },
         ]
     },
     {
-        question: "16 + 1",
+        question: "What geometric shape is generally used for stop signs?",
         options: [
             { id: 'o1', option: '7', isCorrect: false },
             { id: 'o2', option: '37', isCorrect: false },
-            { id: 'o3', option: '17', isCorrect: true },
+            { id: 'o3', option: 'Octagon', isCorrect: true },
             { id: 'o4', option: '27', isCorrect: false },
         ]
     },
     {
-        question: "16 + 9",
+        question: "Which animal can be seen on the Porsche logo?",
         options: [
             { id: 'o1', option: '7', isCorrect: false },
             { id: 'o2', option: '37', isCorrect: false },
-            { id: 'o3', option: '28', isCorrect: true },
+            { id: 'o3', option: 'Horse', isCorrect: true },
             { id: 'o4', option: '27', isCorrect: false },
         ]
     },
+    {
+        question: "What is the name of the World's largest ocean?",
+        options: [
+            { id: 'o1', option: '7', isCorrect: false },
+            { id: 'o2', option: '37', isCorrect: false },
+            { id: 'o3', option: 'Pacific Ocean', isCorrect: true },
+            { id: 'o4', option: '27', isCorrect: false },
+        ]
+    },
+    {
+        question: "What was the first soft drink in space?",
+        options: [
+            { id: 'o1', option: '7', isCorrect: false },
+            { id: 'o2', option: 'Coca Cola', isCorrect: true },
+            { id: 'o3', option: '17', isCorrect: false },
+            { id: 'o4', option: '27', isCorrect: false },
+        ]
+    },  {
+        question: "What is the most consumed manufactured drink in the world?",
+        options: [
+            { id: 'o1', option: '7', isCorrect: false },
+            { id: 'o2', option: '37', isCorrect: false },
+            { id: 'o3', option: 'Tea', isCorrect: true },
+            { id: 'o4', option: '27', isCorrect: false },
+        ]
+    },  
 ]
